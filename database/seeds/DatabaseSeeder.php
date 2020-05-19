6e75db6169922e94e57d76f7b0f20e8692daa6d3
Joseph Silber <contact@josephsilber.com>
2020-05-19 10:00:43 -0400

    Add setup provided in issue

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,82 @@
 <?php
 
+use App\Account;
+use App\CcrEvent;
+use App\CcrTeam;
+use App\Member;
+use App\Team;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        /*
+        * Admin role and permissions
+        */
+        Bouncer::role()->firstOrCreate(
+            [
+                'name'  => 'admin',
+                'title' => 'Administrateur',
+            ]
+        );
+        Bouncer::allow('admin')->everything();
+
+        /*
+        * Responsible role and permissions
+        */
+        Bouncer::role()->firstOrCreate(
+            [
+                'name'  => 'responsible',
+                'title' => 'Responsable d\'association',
+            ]
+        );
+        Bouncer::allow('responsible')->to('index', Account::class);
+        Bouncer::allow('responsible')->to('index', CcrEvent::class);
+        Bouncer::allow('responsible')->to('index', CcrTeam::class);
+        Bouncer::allow('responsible')->to('index', Member::class);
+        Bouncer::allow('responsible')->to('index', Team::class);
+
+        /*
+        * Coach role and permissions
+        */
+        Bouncer::role()->firstOrCreate(
+            [
+                'name'  => 'coach',
+                'title' => 'Entraîneur en chef',
+            ]
+        );
+        Bouncer::allow('coach')->to('index', Member::class);
+        Bouncer::allow('coach')->to('index', Team::class);
+
+        /*
+        * Referee role
+        */
+        Bouncer::role()->firstOrCreate(
+            [
+                'name'  => 'referee',
+                'title' => 'Arbitre',
+            ]
+        );
+        Bouncer::allow('referee')->to('referee', CcrEvent::class);
+
+        /*
+        * Extra abilities
+        */
+        Bouncer::ability()->firstOrCreate(
+            [
+                'name'  => 'can-access-panel',
+                'title' => 'Can access panel',
+            ]
+        );
+
+        factory(User::class, 1000)->create();
+
+        $users = User::where('is_superuser', 1)->get();
+
+        foreach ($users as $user) {
+            $user->allow('can-access-panel');
+        }
     }
 }
